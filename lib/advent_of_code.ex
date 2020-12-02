69cb381dafd7e31a6ad9c1568c47dd65f08e37d3
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-12-02 01:03:40 -0500

    Add specs

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -16,14 +16,22 @@ defmodule AdventOfCode do
     :world
   end
 
+  @spec read_file(
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | char,
+              binary | []
+            )
+        ) :: [binary]
   def read_file(file) do
     :advent_of_code
-      |> :code.priv_dir()
-      |> Path.join(file)
-      |> File.read!()
-      |> String.split(["\n"])
+    |> :code.priv_dir()
+    |> Path.join(file)
+    |> File.read!()
+    |> String.split(["\n"])
   end
 
+  @spec boolean_to_integer(boolean) :: 0 | 1
   def boolean_to_integer(true), do: 1
   def boolean_to_integer(false), do: 0
 end
