64529526214fcf68d587ebd1f33e932ec99779b8
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-12-06 11:59:29 -0500

    Add Day 6 solutions

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -31,6 +31,21 @@ defmodule AdventOfCode do
     |> String.split(["\n"])
   end
 
+  def chunk_by_empty(element, acc) do
+    if element == "" do
+      {:cont, Enum.reverse(acc), []}
+    else
+      {:cont, [element | acc]}
+    end
+  end
+
+  def chunk_after(acc) do
+    case acc do
+      [] -> {:cont, []}
+      acc -> {:cont, Enum.reverse(acc), []}
+    end
+  end
+
   @spec boolean_to_integer(boolean) :: 0 | 1
   def boolean_to_integer(true), do: 1
   def boolean_to_integer(false), do: 0
---
+++ lib/day6.ex
@@ -0,0 +1,31 @@
+defmodule AdventOfCode.Day6 do
+  def day6() do
+    "day6_input"
+    |> AdventOfCode.read_file()
+    |> Enum.chunk_while([], &AdventOfCode.chunk_by_empty/2, &AdventOfCode.chunk_after/1)
+    |> Enum.map_reduce({0, 0}, fn group, {num_union, num_intersection} ->
+      {group_union, group_intersection, _} =
+        Enum.reduce(group, {MapSet.new(), MapSet.new(), true}, fn person_ans,
+                                                                  {union, intersection, first} ->
+          person_set =
+            person_ans
+            |> String.graphemes()
+            |> MapSet.new()
+
+          union = MapSet.union(person_set, union)
+
+          intersection =
+            case first do
+              true -> person_set
+              false -> MapSet.intersection(person_set, intersection)
+            end
+
+          {union, intersection, false}
+        end)
+
+      total_union = MapSet.size(group_union) + num_union
+      total_intersection = MapSet.size(group_intersection) + num_intersection
+      {{group_union, group_intersection}, {total_union, total_intersection}}
+    end)
+  end
+end
